7da3bb8fcac35368ccd4448e973ae09688e8f6f6
Brian <brianezequielv2004@gmail.com>
2023-11-09 19:48:22 -0300

    creando las vistas

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -4,20 +4,22 @@ import '../../public/css/navbar.css';
 export const NavBar = () => {
   return (
     <nav>
-      <div className="left">
+      <div className='logoDiv'>
         <h1>Musicboxd</h1>
       </div>
-      <div className="center">
+      <div className='linksDiv'>
         <ul>
           <li><a href="#">Releases</a></li>
           <li><a href="#">Artists</a></li>
         </ul>
       </div>
-      <div className="right">
+      <div className='searchDiv'>
         <form>
           <input type="text" placeholder="Search..."/>
           <button type="submit">Search</button>
         </form>
+      </div>
+      <div className='signDiv'>
         <a href="#">Sign In</a>
       </div>
     </nav>
