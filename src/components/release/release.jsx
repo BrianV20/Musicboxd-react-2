787c6c7e079f0f5e24d4d90443ef5fb320c494c5
alexislan <alexislanchiotti13@gmail.com>
2023-11-10 09:21:01 -0300

    ya no quiero hacer mas nada, creo que no falta nda mas

--- src/components/release/release.jsx
+++ src/components/release/release.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 // import './Body.css';
+import { createReviewSchema  } from "@/schemas/validation/review";
+import { mappedErrors } from "@/utils/mapped-errors";
 import { getReleases } from '../../services/releases';
-import { getReviews } from '../../services/reviews';
+import { getReviews, createReview} from '../../services/reviews';
 import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useLocation } from "wouter";
 
 export const Release = () => {
-  const albumData = {
-    title: 'Artaud',
-    artist: 'Pescado Rabioso',
-    genre: 'Argentinian Rock',
-    year: '1971',
-    cover: 'https://images.squarespace-cdn.com/content/v1/5a0dd6831f318dcf5130a0d5/4ab03905-7a61-403d-909d-2a2ba0dfafd1/ARTAUD_1080x.jpeg', 
-    description: 'Este álbum es impresionante, pero los normis lo infravaloran. Me gusta como suena la ocarina',
-  };
+  const [errors, setErrors] = useState({});
+  const queryClient = useQueryClient();
+  const [, setNavigate] = useLocation();
+  // const albumData = {
+  //   title: 'Artaud',
+  //   artist: 'Pescado Rabioso',
+  //   genre: 'Argentinian Rock',
+  //   year: '1971',
+  //   cover: 'https://images.squarespace-cdn.com/content/v1/5a0dd6831f318dcf5130a0d5/4ab03905-7a61-403d-909d-2a2ba0dfafd1/ARTAUD_1080x.jpeg', 
+  //   description: 'Este álbum es impresionante, pero los normis lo infravaloran. Me gusta como suena la ocarina',
+  // };
+  const { mutate } = useMutation({
+    mutationKey: ["review"],
+    mutationFn: (reviewData) => createReview(reviewData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["review"] });
+      toast.success("review created");
+      setNavigate("/");
+    },
+    onError: (error) => {
+      console.log(error)
+      toast.error("Error creating user");
+    },
+  });
   const {
     data: releases
   } = useQuery({
@@ -28,28 +49,47 @@ export const Release = () => {
   // if (isError) toast.error("Error getting users");
 
   // const [reviews, setReviews] = useState([]);
-  const [newReview, setNewReview] = useState('');
-  const [userName, setUserName] = useState('');
+  // const [newReview, setNewReview] = useState('');
+  // const [userName, setUserName] = useState('');
 
-  const handleAddReview = () => {
-    if (newReview.trim() !== '' && userName.trim() !== '') {
-      const userReview = `${userName}: ${newReview}`;
-      setReviews([...reviews, userReview]);
-      setNewReview('');
-      setUserName('');
+  // const handleAddReview = () => {
+  //   if (newReview.trim() !== '' && userName.trim() !== '') {
+  //     const userReview = `${userName}: ${newReview}`;
+  //     setReviews([...reviews, userReview]);
+  //     setNewReview('');
+  //     setUserName('');
+  //   }
+  // };
+  const handleAddReview = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const formData = new FormData(e.target);
+    const data = {
+        ...Object.fromEntries(formData),
+        UserId: 3,
+        ReleaseId: parseInt(formData.get('releaseId'), 10),
+        RatingId: parseInt(formData.get('ratingId'), 10),
+      };
+    const { success, errors } = mappedErrors(createReviewSchema, data);
+    if (!success) {
+      console.log(errors);
+      setErrors(errors);
+      return;
     }
+    e.target.reset();
+    setErrors({});
+    mutate(data);
   };
-
   // Reseña de prueba
   // const pruebaReview = 'NoMamesCabron8825: Esta es una reseña de prueba para destacar la funcionalidad. ¡Gran álbum!';
 
   return (
     <main id='mainRelease'>
       <div className="album-container">  
-      {/* <img src={albumData.cover} alt="Portada del Álbum" className="album-cover" />  ahora agrego este campo*/}
         {releases?.map((release) => (
-      <div>
+          <div>
         <div className="album-details">
+        <img src={release.UrlImage} alt="Portada del Álbum" className="album-cover" />
           <h1>{release.name}</h1>
           <h2>por {release.artist.fullName}</h2>
           <h6>{release.genre.name}</h6>
@@ -65,14 +105,30 @@ export const Release = () => {
 
             </ul>
               ))}
-            <div className="review-input">
-              <textarea
-                placeholder="Escribe tu reseña aquí..."
-                value={newReview}
-                onChange={(e) => setNewReview(e.target.value)}
-              />
-              <button onClick={handleAddReview}>Agregar reseña</button>
-            </div>
+            <form onSubmit={handleAddReview}>
+
+              <div className="review-input">
+                <textarea
+                  placeholder="Escribe tu reseña aquí..."
+                  name='Text'
+                  // value={newReview}
+                  // onChange={(e) => setNewReview(e.target.value)}
+                />
+                <input 
+                type="number" 
+                name='ratingId'
+                placeholder='ingrese su calif (1 a 5)'
+                />
+                <input 
+                type="number" 
+                name='releaseId'
+                value={release.id}
+                placeholder='ingrese su calif (1 a 5)'
+                />
+                {/* hay que ocultar este input */}
+              </div>
+                <button>Agregar reseña</button>
+            </form>
           </div>
       </div>
         
--- src/schemas/validation/review.js
+++ src/schemas/validation/review.js
@@ -2,9 +2,10 @@ import { z } from "zod";
 
 export const createReviewSchema = z
     .object({
-        Text: z.string().min(1).max(255).required(),
-        UserId: z.number().min(1).required(),
-        ReleaseId: z.number().min(1).required(),
+        Text: z.string().min(1).max(255).nullable(),
+        UserId: z.number().min(1).nullable(),
+        ReleaseId: z.number().min(1).nullable(),
+        RatingId: z.number().min(1).nullable()
     });
 
 export const updateReviewSchema = z
